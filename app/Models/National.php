f847649e1e2553a7f9c35b92c8270b4a0b8eeaba
Seagulls-H <134987887+Seagulls-H@users.noreply.github.com>
2023-06-18 14:12:08 +0700

    add relationship national
    add relationship national

--- app/Models/National.php
+++ app/Models/National.php
@@ -10,4 +10,8 @@ class National extends Model
     use HasFactory;
     protected $table = 'nationals';
     protected $fillable = ['name'];
+    public function songs()
+    {
+        return $this->hasMany(Song::class);
+    } 
 }
