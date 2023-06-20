b7b622fa430c61709d8b3bcbb19932c34a879b2a
kientran315 <trungkientran333@gmail.com>
2023-06-20 10:36:15 +0700

    update route for authentication

--- routes/web.php
+++ routes/web.php
@@ -26,3 +26,8 @@ Route::resource('/artists', ArtistController::class);
 Route::resource('/categories', CategoryController::class);
 Route::resource('/nationals', NationalController::class);
 Route::resource('/albums', AlbumController::class);
+Route::get('/login', AuthenticationController::class . '@loginIndex');
+Route::get('/register', AuthenticationController::class . '@registerIndex');
+Route::post('/login', AuthenticationController::class . '@login');
+Route::post('/register', AuthenticationController::class . '@register');
+Route::get('/logout', AuthenticationController::class . '@logout');
